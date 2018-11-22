b2d1a2b347e7fb96b3240b4a86bf505544ecf5a8
Rei Yamashiro <e185738@ie.u-ryukyu.ac.jp>
2018-11-22 16:47:38 +0900

    step2 commit

--- src/Report.java
+++ src/Report.java
@@ -1,6 +1,11 @@
 public class Report {
-    public static void main(String[] args){
-        String str = null;
-        System.out.println(str.length());
+    public static void main(String[] args) {
+        try{
+            String str = null;
+            System.out.println(str.length());
+        } catch (NullPointerException e){
+            System.out.println("NullPointerExceptionが発生しました。");
+            System.out.println(e.getMessage());
+        }
     }
 }
